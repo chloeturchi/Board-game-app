832b5ff2a17d60a8623a6ea5f8e687742136cb73
Chloe <chloeturchi@gmail.com>
2019-10-16 13:13:40 +0200

    move roght ok

--- js/main.js
+++ js/main.js
@@ -3,42 +3,194 @@ import Obstacle from './obstacle.js'
 import Weapon from './weapon.js'
 import Character from './character.js'
 
-/******************* Generate grid divs and 2D array */
-const gameMap = new Map (10);
-gameMap.generateGrid("#wrapper");
-const gridContent = gameMap.createArray(10,10);
+/************************************* ETAPE 1 *************************************/
 
-/******************** Obstacles */
+////////////////////////////// GENERATE GRID DIVS AND 2DARRAY //////////////////////////////
+const game_map = new Map (10);
+game_map.generateGrid("#wrapper");
+const grid_content = game_map.createArray(10,10);
+
+////////////////////////////// OBSTACLES //////////////////////////////
 // Put obstacles in an array
-const obstacleArray = [];
-
-function obstacleInArray (number){
+const obstacle_arr = [];
+function pushObstacles (number) {
     for (let i = 0; i < number; i++) {
-        const newObstacle = new Obstacle(i);
-        obstacleArray.push(newObstacle);
+        const obstacle = new Obstacle(i);
+        obstacle_arr.push(obstacle);
     };  
-} 
-obstacleInArray(8);
+} pushObstacles(8);
 
-// Add obstacles array & display in grid
-for (let i = 0; i < obstacleArray.length; i++){
-    function obstacleRandom(){
-        const randomRow = randomNumber();
-        const randomCol = randomNumber();
-        if (gridContent[randomRow][randomCol] == undefined) {
-            gridContent[randomRow][randomCol] = obstacleArray[i];
-            $("#grid-cell-" + randomRow + "-" + randomCol).addClass("obstacle");
-            $("#grid-cell-" + randomRow + "-" + randomCol).append("<img class='obstacleImg' src='assets/img/obstacle.svg'/>");
+// Add obstacles in grid array & display 
+for (let i = 0; i < obstacle_arr.length; i++){
+    function displayObstacles(){
+        const random_row = randomNumber();
+        const random_col = randomNumber();
+        console.log(random_row, random_col);
+        if (grid_content[random_row][random_col] == undefined) {
+            grid_content[random_row][random_col] = obstacle_arr[i];
+            $("#grid-cell-" + random_row + "-" + random_col).addClass("obstacle");
+            $("#grid-cell-" + random_row + "-" + random_col).append("<img class='obstacleImg' src='assets/img/obstacle.svg'/>");
         } else {
-            obstacleRandom();
+            displayObstacles();
         }
-    } obstacleRandom();
+    };  displayObstacles()
 };
 
-/******************** Functions */
+////////////////////////////// WEAPONS //////////////////////////////
+// Instances of Weapon
+const dagger = new Weapon("dagger", 10);
+const bow = new Weapon("bow", 20);
+const axe = new Weapon("axe", 30);
+const sword = new Weapon("sword", 40);
+
+// Add weapons in an array
+const weapon_arr = [];
+weapon_arr.push.apply(weapon_arr, [bow, axe, sword]);
+
+// Add weapon in grid array & display 
+for (let i = 0; i < weapon_arr.length; i++){
+    function displayWeapons(){
+        const random_row = randomNumber();
+        const random_col = randomNumber();
+        if (grid_content[random_row][random_col] == undefined) {
+            grid_content[random_row][random_col] = weapon_arr[i];
+            $("#grid-cell-" + random_row + "-" + random_col).addClass("weapon");
+            if (weapon_arr[i].name == 'bow'){
+                $("#grid-cell-" + random_row + "-" + random_col).append("<img class='weaponImg' id='bowImg' src='assets/img/bow.svg'/>"); 
+            } else if (weapon_arr[i].name == 'axe'){
+                $("#grid-cell-" + random_row + "-" + random_col).append("<img class='weaponImg' id='axeImg' src='assets/img/axe.svg'/>"); 
+            } else if (weapon_arr[i].name == 'sword'){
+                $("#grid-cell-" + random_row + "-" + random_col).append("<img class='weaponImg' id='swordImg' src='assets/img/sword.svg'/>"); 
+            }
+        } else {
+            displayWeapons();
+        }
+    };  displayWeapons();
+};
+
+////////////////////////////// CHARACTERS //////////////////////////////
+// Instances of Character
+const character1 = new Character("Combattant 1", 100);
+const character2 = new Character("Combattant 2", 100);
+
+// Add characters in an array
+const character_arr = [];
+character_arr.push.apply(character_arr, [character1, character2]);
+
+// Characters positions on grid
+let character1_pos = [];
+let character2_pos = [];
+
+// Add characters in grid array
+for (let i = 0; i < character_arr.length; i++){
+    function displayCharacter(){
+        const random_row = randomNumber();
+        const random_col = randomNumber();
+        // Test if value around Character in Grid Array has Obstacle or character
+        if (random_row - 1 !== -1 && grid_content[random_row - 1][random_col] !== undefined) {
+            displayCharacter();
+        } else if (random_row + 1 !== grid_content.length && grid_content[random_row + 1][random_col] !== undefined) {
+            displayCharacter();
+        } else if (random_col - 1 !== -1 && grid_content[random_row][random_col - 1] !== undefined) {
+            displayCharacter();
+        } else if (random_col + 1 !== grid_content.length && grid_content[random_row][random_col + 1] !== undefined) {
+            displayCharacter();
+        // Test if the chosen Character area is undefined
+        } else if (grid_content[random_row][random_col] == undefined){
+            grid_content[random_row][random_col] = character_arr[i];
+            if (character_arr[i].name == 'Combattant 1'){
+                character1_pos.push.apply(character1_pos, [[random_row][0], [random_col][0]]);
+                $("#grid-cell-" + random_row + "-" + random_col).append("<img class='characterImg' id ='character1Img' src='assets/img/perso1.svg'/>"); 
+                $("#grid-cell-" + random_row + "-" + random_col).addClass("character1");
+            } else if (character_arr[i].name == 'Combattant 2'){
+                character2_pos.push.apply(character2_pos, [[random_row][0], [random_col][0]]);
+                $("#grid-cell-" + random_row + "-" + random_col).append("<img class='characterImg' id ='character2Img' src='assets/img/perso2.svg'/>"); 
+                $("#grid-cell-" + random_row + "-" + random_col).addClass("character2");
+            }
+        } else {
+            displayCharacter();
+        }
+    } displayCharacter();
+};
+
+/************************************* ETAPE 2 *************************************/
+
+////////////////////////////// OBSTACLES //////////////////////////////
+// MOVE RIGHT
+for (let i = 0; i < 3; i++){
+    let row = character1_pos[0] 
+    let col = character1_pos[1] + (i + 1)
+    if (col < grid_content.length) {
+        if (grid_content[row][col] instanceof Weapon) {
+            if (grid_content[row][col].name === 'bow'){
+                weaponOnClick ("#grid-cell-" + row + "-" + col, 'img#bowImg', row, col);
+            } else if (grid_content[row][col].name === 'axe'){
+                weaponOnClick ("#grid-cell-" + row + "-" + col, 'img#axeImg', row, col);
+            } else if (grid_content[row][col].name === 'sword'){
+                weaponOnClick ("#grid-cell-" + row + "-" + col, 'img#swordImg', row, col);
+            }
+        } else if (grid_content[row][col] == undefined){
+            undefinedOnClick ("#grid-cell-" + row + "-" + col, row, col);
+        } else {
+            break;
+        }
+    }  
+};
+
+////////////////////////////// FUNCTIONS & LOGS //////////////////////////////
 // Create random Number
 function randomNumber(){
-    return Math.floor(Math.random() * gridContent.length)
+    return Math.floor(Math.random() * grid_content.length)
 }
 
-console.log(gridContent);
+// What to do when click on a weapon
+function weaponOnClick(div, img_to_remove, row, col) {
+    $(div).click(function(){
+        grid_content[row][col] = character1;
+        grid_content[character1_pos[0]][character1_pos[1]] = undefined;
+        $(img_to_remove).remove();  
+        changeDom(div);
+        checkFight();
+    })
+};
+
+// What to do when click on an undefined 
+function undefinedOnClick (div, row, col) {
+    $(div).addClass("highlight");
+    $(div).click(function(){
+        grid_content[row][col] = character1;
+        grid_content[character1_pos[0]][character1_pos[1]] = undefined;
+        changeDom(div);
+        checkFight();
+    })
+};
+
+// Function moveOnClick
+function changeDom(divs) {
+    $('img#character1Img').remove();
+    $('div').removeClass('character1');
+    $(divs).addClass('character1');
+    $(divs).append("<div class='character1 square'><img class='characterImg' id ='character1Img' src='assets/img/perso1.svg'/></div>");
+    $('div').removeClass('highlight');
+}
+
+// Function check for fight
+function checkFight(){
+    for (let row = 0; row < grid_content.length; row++) {
+        let column = grid_content[row];
+        for (let col = 0; col < column.length; col++) {
+            if (column[col] instanceof Character && column[col].name =='Combattant 1'){
+                if (row + 1 !== grid_content.length && grid_content[row + 1][col] instanceof Character) {
+                    console.log('FIGHT');
+                } else if (row - 1 !== grid_content.length && grid_content[row - 1][col] instanceof Character){
+                    console.log('FIGHT');
+                } else if (col + 1 !== grid_content.length && grid_content[row][col + 1] instanceof Character){
+                    console.log('FIGHT');   
+                } else if (col - 1 !== grid_content.length && grid_content[row][col - 1] instanceof Character){
+                    console.log('FIGHT');
+                }
+            }
+        }
+    }
+};
+
--- js/map.js
+++ js/map.js
@@ -2,31 +2,35 @@
 
 export default class Map { 
   
-  constructor(gridSize) {
-    this.gridSize = gridSize;
+  constructor(grid_size) {
+    this.grid_size = grid_size;
   }
   
   generateGrid(parentID) {
-    for (let i = 0; i < this.gridSize; i++) {
-      let newRowDiv = document.createElement("div");
-      newRowDiv.setAttribute("id", `grid-row-${i}`);
-      newRowDiv.setAttribute("class", "grid-row");
-      for (let x = 0; x < this.gridSize; x++) {
-        let newColDiv = document.createElement("div");
-        newColDiv.setAttribute("id", `grid-cell-${i}-${x}`);
-        newColDiv.setAttribute("class", "grid-cell");
-        newRowDiv.appendChild(newColDiv);
+    for (let i = 0; i < this.grid_size; i++) {
+      let new_row_div = document.createElement("div");
+      new_row_div.setAttribute("id", `grid-row-${i}`);
+      new_row_div.setAttribute("class", "grid-row");
+      for (let x = 0; x < this.grid_size; x++) {
+        let new_col_div = document.createElement("div");
+        new_col_div.setAttribute("id", `grid-cell-${i}-${x}`);
+        new_col_div.setAttribute("class", "grid-cell");
+        new_row_div.appendChild(new_col_div);
       }
-      $(parentID).append(newRowDiv);
+      $(parentID).append(new_row_div);
     }
   }
+
+  addCell(grid, row, col) {
+    grid[row][col] = undefined; 
+  };
 
   createArray(rowCount, colCount) {
     const grid = [];
     for (let row = 0; row < rowCount; row++) {
       grid[row] = [];
       for (let col = 0; col < colCount; col++) {
-        grid[row][col] = undefined; 
+        this.addCell(grid, row, col);
       }
     }  return grid;
   }
